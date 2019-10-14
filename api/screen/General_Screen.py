4f39f398472b89e10f0bfd36c5d6ca32eab8b5e0
Pedro Nobrega <pedro.nobrega@agilems.com>
2019-10-14 01:14:03 -0300

    type: refactor description: change 'open' method to general_screen
    Signed-off-by: Pedro Nobrega <pedro.nobrega@agilems.com>

--- api/screen/General_Screen.py
+++ api/screen/General_Screen.py
@@ -16,9 +16,28 @@ class GeneralScreen(ABC):
     def controller(self, controller: ABCController):
         self.__controller = controller
 
-    @abstractmethod
     def open(self):
-        pass
+        print(" User Screen ".center(60, "-"))
+        print(" | Você Deseja:  | ".center(60))
+        print(" | Inserir [1]  | ".center(60))
+        print(" | Deletar [2]  | ".center(60))
+        print(" | Editar [3]  | ".center(60))
+        print(" | Listar [4]  | ".center(60))
+        print(" | Voltar Para o Menu[5] | ".center(60))
+        while True:
+            option = int(input(" Insira a opção desejada: ".center(60)))
+            if option == 1:
+                self.open_add_menu()
+            elif option == 2:
+                self.open_delete_menu()
+            elif option == 3:
+                self.open_edit_menu()
+            elif option == 4:
+                self.open_list_menu()
+            elif option == 5:
+                self.open_main_screen()
+            else:
+                print("Opção Inválida".center(60, "-"))
 
     @abstractmethod
     def open_add_menu(self):
