ee2fc0aaaa90b8605da58538deda6919f171e4e6
Pedro Nobrega <pedro.nobrega@agilems.com>
2019-10-15 20:58:46 -0300

    type: refactor description: make it run from bash
    Signed-off-by: Pedro Nobrega <pedro.nobrega@agilems.com>

--- api/controller/__init__.py
+++
@@ -1,6 +0,0 @@
-from api.controller.Main_Controller import MainController
-
-
-if __name__ == "__main__":
-    controller = MainController()
-    controller.main_screen.open()
---
+++ start.py
@@ -0,0 +1,6 @@
+from api.controller.Main_Controller import MainController
+
+
+if __name__ == "__main__":
+    controller = MainController()
+    controller.main_screen.open()
